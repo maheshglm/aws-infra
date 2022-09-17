804f3b0c07b2090706b2e2c0f92c98d3dd37addb
maheshglm <maheshglm@gmail.com>
2022-09-17 22:42:54 +0800

    Updated vpc module with private and public subnet tags

--- modules/vpc/v3.14.2/main.tf
+++ modules/vpc/v3.14.2/main.tf
@@ -1,11 +1,11 @@
 data "aws_availability_zones" "available" {}
 
 locals {
-  azs     = data.aws_availability_zones.available.names
+  azs  = data.aws_availability_zones.available.names
   tags = {
-    customer  = var.customer,
-    project   = var.project,
-    env       = var.environment
+    customer = var.customer,
+    project  = var.project,
+    env      = var.environment
   }
 }
 
@@ -13,24 +13,27 @@ module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "v3.14.2"
 
-  name                              = format("%s-%s-%s", var.customer, var.project, var.environment)
-  cidr                              = var.cidr
-  azs                               = data.aws_availability_zones.available.names
-  private_subnets                   = var.private_subnets
-  public_subnets                    = var.public_subnets
-  database_subnets                  = var.database_subnets
-  enable_vpn_gateway                = var.enable_vpn_gateway
-  enable_dns_support                = var.enable_dns_support
-  enable_dns_hostnames              = var.enable_dns_hostnames
-  create_database_subnet_group      = var.create_database_subnet_group
+  name                         = format("%s-%s-%s", var.customer, var.project, var.environment)
+  cidr                         = var.cidr
+  azs                          = data.aws_availability_zones.available.names
+  private_subnets              = var.private_subnets
+  public_subnets               = var.public_subnets
+  database_subnets             = var.database_subnets
+  enable_vpn_gateway           = var.enable_vpn_gateway
+  enable_dns_support           = var.enable_dns_support
+  enable_dns_hostnames         = var.enable_dns_hostnames
+  create_database_subnet_group = var.create_database_subnet_group
 
-  enable_nat_gateway     =  var.enable_nat_gateway
-  single_nat_gateway     =  var.single_nat_gateway
-  one_nat_gateway_per_az =  var.one_nat_gateway_per_az
+  enable_nat_gateway     = var.enable_nat_gateway
+  single_nat_gateway     = var.single_nat_gateway
+  one_nat_gateway_per_az = var.one_nat_gateway_per_az
 
   default_vpc_tags = merge({
     Terraform = "true"
   },
   local.tags
   )
+
+  private_subnet_tags = var.private_subnet_tags
+  public_subnet_tags  = var.public_subnet_tags
 }
